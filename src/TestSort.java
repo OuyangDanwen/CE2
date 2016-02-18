9c9cd6640941afb8cdeab897a667c3d716518b46
Ouyang Danwen <aaron117joy@gmail.com>
2016-02-18 21:17:29 +0800

    Edited TestSort

--- src/TestSort.java
+++ src/TestSort.java
@@ -1,11 +1,10 @@
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 
 import org.junit.Test;
 
 public class TestSort {
-	
+
 
 	@Test
 	public void test() {
@@ -13,15 +12,16 @@ public class TestSort {
 		TextBuddy.backupListForEasyDeletion.add("pair");
 		TextBuddy.backupListForEasyDeletion.add("cat");
 		TextBuddy.backupListForEasyDeletion.add("POP");
-		TextBuddy.sort();
-		
+		TextBuddy.executeUserCommand("sort","sort");
+
 		ArrayList<String> expected = new ArrayList<String>();
 		expected.add("cat");
 		expected.add("pair");
 		expected.add("POP");
-		
+
 		assertEquals(expected,TextBuddy.backupListForEasyDeletion);
-		
+		TextBuddy.backupListForEasyDeletion.clear();
+
 	}
 
 }
